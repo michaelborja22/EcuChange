9b169ab04f110fdab923a718355ad1ba94875cbb
pichajr22 <maicol2014gb@gmail.com>
2022-02-05 12:22:04 -0500

    Actualizacion de login botones info

--- app/src/main/java/com/example/ecuchange/Login.kt
+++ app/src/main/java/com/example/ecuchange/Login.kt
@@ -5,10 +5,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.example.ecuchange.databinding.ActivityLoginBinding
 import com.google.android.material.textfield.TextInputLayout
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+    super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.botonLogin.setOnClickListener(){
+            if(binding.txtEmail.text.toString().trim()==""){
+                binding.layoutEmail.error = getString(R.string.error)
+            }else{
+                binding.layoutEmail.error=""
+                var intent = Intent(this, PrincipalActivity::class.java)
+                startActivity(intent)
+            }
+
+
+        }
+
+        binding.txtPolPri.setOnClickListener(){
+            Toast.makeText(this,"APLICACION PROPIEDAD DE MICHAEL BORJA Y ALEX TONATO. CONTACT US",Toast.LENGTH_LONG).show()
+        }
+
+
+/*
+Antes
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         var botonLogin = findViewById<Button>(R.id.botonLogin)
@@ -24,7 +53,7 @@ class Login : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-
+*/
 
     }
 }
--- app/src/main/java/com/example/ecuchange/PrincipalActivity.kt
+++ app/src/main/java/com/example/ecuchange/PrincipalActivity.kt
@@ -2,10 +2,24 @@ package com.example.ecuchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecuchange.databinding.ActivityPrincipalBinding
 
 class PrincipalActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPrincipalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
+        binding = ActivityPrincipalBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnClickListener(){
+            binding.textView9.text="DA"
+        }
+
+
     }
+
+
 }
