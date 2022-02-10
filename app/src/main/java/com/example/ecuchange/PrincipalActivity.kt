6f28e4d6854c6afd7b8989c0d54920ccadfbe353
pichajr22 <maicol2014gb@gmail.com>
2022-02-10 10:36:24 -0500

    Nada

--- app/src/main/java/com/example/ecuchange/PrincipalActivity.kt
+++ app/src/main/java/com/example/ecuchange/PrincipalActivity.kt
@@ -3,12 +3,14 @@ package com.example.ecuchange
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import com.example.ecuchange.databinding.ActivityPrincipalBinding
 import androidx.fragment.app.Fragment as Fragment
 
 class PrincipalActivity : AppCompatActivity() {
 
     private lateinit var binding: com.example.ecuchange.databinding.ActivityPrincipalBinding
+    private var lstFragments = mutableListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +33,18 @@ class PrincipalActivity : AppCompatActivity() {
                     binding.bottomNavigation.getOrCreateBadge(R.id.botonGusta).isVisible=false
                     // An icon only badge will be displayed unless a number is set:
                     createFragment(HomeFragment())
+                    lstFragments.add(R.id.botonInicio)
                     true
                 }
 
                 R.id.botonGusta -> {
                     createFragment(LikeFragment())
+                    lstFragments.add(R.id.botonGusta)
                     true
                 }
                 R.id.botonChat -> {
                     createFragment(ChatFragment())
+                    lstFragments.add(R.id.botonChat)
                     true
                 }
                 else -> false
@@ -49,12 +54,22 @@ class PrincipalActivity : AppCompatActivity() {
 
     }
 
+    override fun OnBackPressed(){
+        super.onBackPressed()
+        if(lstFragments.isNotEmpty()){
+        lstFragments.removeLast()
+            binding.bottomNavigation.menu.findItem(lstFragments.last()).setChecked(true)
+        }
+    }
+
     fun createFragment(fragment: Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(binding.FrameLayout.id,fragment)
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
 }
+
+
 
     }
 
