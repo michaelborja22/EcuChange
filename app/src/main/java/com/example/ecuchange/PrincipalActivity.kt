e1c059e606ae8d337b536548a11d74c67552eaa5
pichajr22 <maicol2014gb@gmail.com>
2022-02-10 10:20:55 -0500

    Fragments iniciados

--- app/src/main/java/com/example/ecuchange/PrincipalActivity.kt
+++ app/src/main/java/com/example/ecuchange/PrincipalActivity.kt
@@ -9,41 +9,37 @@ import androidx.fragment.app.Fragment as Fragment
 class PrincipalActivity : AppCompatActivity() {
 
     private lateinit var binding: com.example.ecuchange.databinding.ActivityPrincipalBinding
-private var listaFragments = mutableListOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
         binding = ActivityPrincipalBinding.inflate(layoutInflater)
-        createFragment(HomeFragment())
+
         setContentView(binding.root)
+
+        binding.bottomNavigation.setOnClickListener(){
+
+        }
 
         //Hacer que las notificacionmes de la barra de navagacion sea visible
         binding.bottomNavigation.getOrCreateBadge(R.id.botonGusta).isVisible=true
         //Dar notificaciones a los icones de la barra de navegacion
         binding.bottomNavigation.getOrCreateBadge(R.id.botonGusta).number=2
 
-        //Empiezo la activity con el fragment home
-
-
         binding.bottomNavigation.setOnItemSelectedListener { item -> when(item.itemId) {
 
                 R.id.botonInicio -> {
                     binding.bottomNavigation.getOrCreateBadge(R.id.botonGusta).isVisible=false
                     // An icon only badge will be displayed unless a number is set:
-                    listaFragments.add(item.itemId)
                     createFragment(HomeFragment())
                     true
                 }
 
                 R.id.botonGusta -> {
-                    listaFragments.add(item.itemId)
                     createFragment(LikeFragment())
                     true
                 }
                 R.id.botonChat -> {
                     createFragment(ChatFragment())
-                    listaFragments.add(item.itemId)
                     true
                 }
                 else -> false
@@ -53,22 +49,12 @@ private var listaFragments = mutableListOf<Int>()
 
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        if(listaFragments.isNotEmpty()) {
-            listaFragments.removeLast()
-            binding.bottomNavigation.menu.findItem(listaFragments.last()).setChecked(true)
-        }
-    }
-
     fun createFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction().apply {
-        replace(binding.FrameLayout.id,fragment)
-        addToBackStack(null)
-        commit()
-        }
-
-    }
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.FrameLayout.id,fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+}
 
     }
 
