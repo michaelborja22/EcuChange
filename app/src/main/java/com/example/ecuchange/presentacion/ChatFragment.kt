8ee801f09661137c0a7f5968a5d7eca0551f51dd
pichajr22 <maicol2014gb@gmail.com>
2022-03-18 09:53:05 -0500

    InicialIbngresoProducto

--- app/src/main/java/com/example/ecuchange/presentacion/ChatFragment.kt
+++ app/src/main/java/com/example/ecuchange/presentacion/ChatFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.ecuchange.databinding.FragmentChatBinding
-import com.example.ecuchange.databinding.FragmentHomeBinding
 
 class ChatFragment : Fragment() {
 
